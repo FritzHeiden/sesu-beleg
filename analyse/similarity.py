fa0b3a1126f8519937dd8f4481d53dcec6b71c07
s0552107 <s0552107@htw-berlin.de>
2018-01-14 16:38:28 +0100

    ohne gensim 1.1

--- analyse/similarity.py
+++ analyse/similarity.py
@@ -1,4 +1,4 @@
-from gensim.models import Word2Vec
+#from gensim.models import Word2Vec
 
 
 class Similarity:
@@ -10,13 +10,14 @@ class Similarity:
             sentences.append(article.get_words().keys())
 
 
-        model = Word2Vec(sentences,min_count = 1)
-        model.save("test.test")
+       # model = Word2Vec(sentences,min_count = 1)
+        #model.save("test.test")
         print (sentences)
 
     @staticmethod
     def similarity(Wort1, Wort2):
-        model = Word2Vec.load("test.test")
-        print( model.wv.similarity(Wort1,Wort2))
+        print("fill")
+        #model = Word2Vec.load("test.test")
+        #print( model.wv.similarity(Wort1,Wort2))
         #print("as")
 
